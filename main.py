dbb2eb6c4c786fd9b497f0db70cfca0af8648016
Koushik919-del <koushikt.cbook@gmail.com>
2025-06-22 12:51:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 import pytesseract
 
 # 👇 ADD THIS LINE HERE if you're on Windows
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mailt\Downloads'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\mailt\Downloads\tesseract.exe'
 
 from deep_translator import GoogleTranslator
 from gtts import gTTS
